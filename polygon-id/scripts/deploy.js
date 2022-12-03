1f3d12fa4d3e094e2213a69e0e5068e2864b3d1e
jveer634 <jaysparx9@gmail.com>
2022-12-04 00:09:14 +0530

    edit: project dependencies

--- polygon-id/scripts/deploy.js
+++ polygon-id/scripts/deploy.js
@@ -1,4 +1,4 @@
-// to deploy the contracts
+const { ethers } = require("hardhat");
 
 async function main() {
     const verifierContract = "ERC721Verifier";
@@ -6,16 +6,16 @@ async function main() {
     const verifierSymbol = "INST";
     const ERC721Verifier = await ethers.getContractFactory(verifierContract);
     const erc721Verifier = await ERC721Verifier.deploy(
-      verifierName,
-      verifierSymbol
+        verifierName,
+        verifierSymbol
     );
-  
+
     await erc721Verifier.deployed();
     console.log(verifierName, " tx hash:", erc721Verifier.address);
-  }
-  main()
+}
+main()
     .then(() => process.exit(0))
     .catch((error) => {
-      console.error(error);
-      process.exit(1);
+        console.error(error);
+        process.exit(1);
     });
