6892f6a25f4e6928ab953057ca6b3bd3204f50f4
Subhradwip Kulavi <88609894+subhradwip01@users.noreply.github.com>
2022-12-03 20:27:25 +0530

    wrap from errors

---
+++ component/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Box,Typography} from "@mui/material"
+const Navbar = () => {
+  return (
+    <Box width="100%" padding={4}>
+        <Typography 
+            variant='h2'
+            fontWeight="bold"
+        >
+            <span style={{
+                color:"#16c2d5"
+            }}>ZK</span>Med
+        </Typography>
+    </Box>
+  )
+}
+
+export default Navbar
---
+++ pages/Login.tsx
@@ -0,0 +1,41 @@
+import React,{useContext} from 'react'
+import {Box} from "@mui/material"
+import Image from "next/image"
+import uni from "../assets/uni.webp"
+import ButtonUI from '../component/Button'
+import {AuthContext} from '../context/AuthContext'
+import Router from 'next/router'
+const Login = () => {
+    const atx=useContext(AuthContext)
+
+  const login= async ()=>{
+    await atx.login();
+    if(!atx.err){
+        console.log(atx.address);
+        if(!atx.userDetails){
+            Router.push("/StudentRegistration")
+        }else{
+            Router.push("/Student")
+        }
+    }else{
+        console.log("Something went wrong");
+    }
+  }
+  
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center" flexDirection="column" height="100%" mt={30}>
+       <Image
+        src={uni}
+        alt="logo"
+        style={{
+            marginBottom:"20px",
+            borderRadius:"20px",
+            width:"500px"
+        }}
+       /> 
+       <ButtonUI onClickHandler={login} name="Login"/>
+    </Box>
+  )
+}
+
+export default Login
