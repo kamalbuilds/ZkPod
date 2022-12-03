8122493d266d099296455eabb0ff70cdd5a454c9
jveer634 <jaysparx9@gmail.com>
2022-12-03 20:32:46 +0530

    edit: 721 contract

--- polygon-id/contracts/ERC721Verifier.sol
+++ polygon-id/contracts/ERC721Verifier.sol
@@ -2,29 +2,31 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/erc721/erc721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 import "./lib/GenesisUtils.sol";
 import "./interfaces/ICircuitValidator.sol";
 import "./verifiers/ZKPVerifier.sol";
 
 contract ERC721Verifier is ERC721, ZKPVerifier {
-
+    using Counters for Counters.Counter;
     uint64 public constant TRANSFER_REQUEST_ID = 1;
 
-    mapping(uint256 => address) public idToAddress;
-    mapping(address => uint256) public addressToId;
+    Counters.Counter private _tokenIds;
 
-    uint256 public TOKEN_AMOUNT_FOR_ISSUING_NFT_PER_ID = 5 * 10**uint(decimals());
+    // mapping(uint256 => address) public idToAddress;
+    // mapping(address => uint256) public addressToId;
 
     constructor(string memory name_, string memory symbol_)
         ERC721(name_, symbol_)
-    {}    
+    {}
 
     function _beforeProofSubmit(
         uint64, /* requestId */
         uint256[] memory inputs,
         ICircuitValidator validator
     ) internal view override {
-        // check that challenge input of the proof is equal to the msg.sender 
+        // check that challenge input of the proof is equal to the msg.sender
         address addr = GenesisUtils.int256ToAddress(
             inputs[validator.getChallengeInputIndex()]
         );
@@ -47,9 +49,10 @@ contract ERC721Verifier is ERC721, ZKPVerifier {
         uint256 id = inputs[validator.getChallengeInputIndex()];
         // execute the airdropping of NFT
         if (idToAddress[id] == address(0)) {
-            super._mint(_msgSender(), TOKEN_AMOUNT_FOR_ISSUING_NFT_PER_ID);
-            addressToId[_msgSender()] = id;
-            idToAddress[id] = _msgSender();
+            super._mint(_msgSender(), _tokenIds.current());
+            _tokenIds.increment();
+            // addressToId[_msgSender()] = id;
+            // idToAddress[id] = _msgSender();
         }
     }
 
@@ -63,4 +66,38 @@ contract ERC721Verifier is ERC721, ZKPVerifier {
             "only identities who provided proof are allowed to receive tokens"
         );
     }
+
+    // ================== converting to sbt ========================
+    function transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual {}
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {}
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {}
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual override {}
+
+    function approve(address to, uint256 tokenId) public virtual override {}
+
+    function setApprovalForAll(address operator, bool _approved)
+        public
+        virtual
+        override
+    {}
 }
