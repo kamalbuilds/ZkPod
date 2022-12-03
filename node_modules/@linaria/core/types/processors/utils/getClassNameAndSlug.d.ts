37dfcbd804fbd8a1cd2005d083f3c5c4c7668fc4
0xinit <28729137+0xinit@users.noreply.github.com>
2022-12-04 04:20:53 +0530

    Removed the node_modules

--- node_modules/@linaria/core/types/processors/utils/getClassNameAndSlug.d.ts
+++
@@ -1,5 +0,0 @@
-import type { IFileContext, IOptions } from './types';
-export default function getClassNameAndSlug(displayName: string, idx: number, options: IOptions, context: IFileContext): {
-    className: string;
-    slug: string;
-};
